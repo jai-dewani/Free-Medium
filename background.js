4de40ab73d0e4606e8f5712fd4ff517d16a018ae
Jai Kumar Dewani <jai.dewani.99@gmail.com>
2020-12-16 23:10:30 +0530

    Remove useless console.logs

--- background.js
+++ background.js
@@ -4,9 +4,7 @@ let matches = [
 ];
 
 chrome.tabs.onCreated.addListener(function(tab){
-    console.log("tab")
     let {id, pendingUrl} = tab; 
-    console.log(id, pendingUrl);
     let flag = false;
     matches.forEach(url => {
         if(pendingUrl.match(url)!=null){
@@ -18,13 +16,3 @@ chrome.tabs.onCreated.addListener(function(tab){
         chrome.windows.create({"url": pendingUrl, "incognito": true});
     }
 })
-
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
-        let url = tabs[0].url;
-        // use `url` here inside the callback because it's asynchronous!
-        console.log(tabs);
-    });
-    
-});
